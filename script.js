0917c857e2520d2b865067af7984627b171470af
luisantonio010 <luisantoniomb03@gmail.com>
2022-03-30 17:35:38 -0400

    Finalized TOP rock,paper,scissor project
    playable only on console

--- script.js
+++ script.js
@@ -1,40 +1,90 @@
 
 const choices = ['rock', 'paper', 'scissors'];
 
-const playerSelection = ''
-const computerSelection = computerPlay();
-
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay(){
     return choices[Math.floor(Math.random() * 3)];
 }
 
+function playerPlay(){
+   let playerChoise =  prompt('choose rock, paper, or scissors');
 
-function startPlay() {
-    if (playerSelection.toLowerCase() === choices[0] && computerSelection === choices[0]) {
-        console.log('tie') ;
-    } else if (playerSelection.toLowerCase() === choices[0]&& computerSelection === choices[1]) {
-        console.log('Sorry, you lost') ;
-    } else if (playerSelection.toLowerCase() === choices[0] && computerSelection === choices[2]) {
-        console.log('congratulations! you won') ; 
-    } else if (playerSelection.toLowerCase === choices[1] && computerSelection === choices[0]) {
-        console.log('congratulations! you won') ;
-    } else if (playerSelection.toLowerCase === choices[1] && computerSelection === choices[1]) {
-        console.log('tie')  ;
-    } else if (playerSelection.toLowerCase === choices[1] && computerSelection === choices[2]) {
-        console.log('Sorry, you lost') ;
-    } else if (playerSelection.toLowerCase === choices[2] && computerSelection === choices[0]) {
-        console.log('Sorry, you lost') ;
-    } else if (playerSelection.toLowerCase === choices[2] && computerSelection === choices[1]) {
-        console.log('congratulations! you won') ;
-    } else if (playerSelection.toLowerCase === choices[2] && computerSelection === choices[2]) {
-        console.log('tie')  ;
-    } 
-        else
-        console.log('something went wrong')
-
+    if (playerChoise.toLowerCase() === 'rock'){
+        return playerChoise = choices[0];
+    } else if (playerChoise.toLowerCase() === 'paper') {
+        return playerChoise = choices[1];
+    } else if (playerChoise.toLowerCase() === 'scissors') {
+        return playerChoise = choices[2];
+    } else {
+       return console.log('something went wrong')
+    }
 }
 
-function game () {
+function playRound() {
+
+    const playerSelection = playerPlay();
+    console.log(`player chose: ${playerSelection}`); 
+    const computerSelection = computerPlay();
+    console.log(`computer chose: ${computerSelection}`); 
     
+    if (playerSelection === choices[0] && computerSelection === choices[0]) {
+        console.log('%cTie!! go again!', 'color:blue') ;
+    } else if (playerSelection === choices[0]&& computerSelection === choices[1]) {
+        console.log('%cCOMPUTER WINS! Paper beats rock', 'color:red') ;
+        computerScore++;
+    } else if (playerSelection === choices[0] && computerSelection === choices[2]) {
+        console.log('%cPLAYER WINS!Rock beats Scissors', 'color:green') ; 
+        playerScore++;
+
+    } else if (playerSelection === choices[1] && computerSelection === choices[0]) {
+        console.log('%cPLAYER WINS! Paper beats Rock', 'color:green') ;
+        playerScore++        
+    } else if (playerSelection === choices[1] && computerSelection === choices[1]) {
+        console.log('%cTie!! go again!', 'color:blue') ;  ;
+    } else if (playerSelection === choices[1] && computerSelection === choices[2]) {
+        console.log('%cCOMPUTER WINS!  Scissors beat paper', 'color:red') ;
+        computerScore++;
+
+    } else if (playerSelection === choices[2] && computerSelection === choices[0]) {
+        console.log('%cCOMPUTER WINS! Rocks beat Scisoors', 'color:red') ;
+        computerScore++;
+    } else if (playerSelection === choices[2] && computerSelection === choices[1]) {
+        console.log ('%cPLAYER WINS! you won! Scisoors beats Paer', 'color:green') ;
+        playerScore++;
+    } else if (playerSelection === choices[2] && computerSelection === choices[2]) {
+        console.log('%cTie!! go again!', 'color:blue') ; ;
+
+    } else {
+        return ('something went wrong')
+    }
 }
+
+function scoreBoard(){
+    console.log(" *******FINAL SCORE*******")
+    console.log(` Player score:    ${playerScore}`)
+    console.log(` Computer score:  ${computerScore}`)
+}
+
+function finalResult(){
+    if (playerScore > computerScore) {
+        console.log('THE HUMAN WINS!')
+    } else if( computerScore > playerScore) {
+        console.log('THE COMPUTERS ARE SMARTER!')
+    } else {
+        console.log('its a tie! go again!')
+    }
+}
+
+function game() {
+    for (let rounds = 0; rounds < 5; rounds++) {     
+      const result = playRound();
+    }          
+    scoreBoard() 
+    finalResult()
+ }
+
+
+
+
